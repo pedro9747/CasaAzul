2202092db227cb4abc919a51562dd93ca5753c7f
Pedro P <pedro9747@gmail.com>
2024-02-27 23:57:50 -0300

    languageImprovement

--- script.js
+++ script.js
@@ -11,6 +11,24 @@ if(navMenu.classList.contains("nav-menu-visible")){
     navToggle.setAttribute("aria-label", "Abrir menu")
 }
 
+//selector de idioma
+const flagsElement = document.getElementById("flags");
+const textsToChange = document.querySelectorAll("[data-section]");
+const changeLanguage = async language=>{
+  const requestJson = await fetch(`languages/${language}.json`);
+  const texts = await requestJson.json();
+  for(const textToChange of textsToChange){
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
+    textToChange.innerHTML = texts[section][value];
+  }
+  
+}
+
+flagsElement.addEventListener("click", (e) =>{
+  changeLanguage(e.target.parentElement.dataset.language);
+})
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
